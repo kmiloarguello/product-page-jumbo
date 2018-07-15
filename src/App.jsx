2c24228befdafd3fef4f1bafe5b8851c449f3ca6
Camilo Arguello <camilo.arguello.rincon@gmail.com>
2018-07-14 19:30:15 -0500

    Adding files and fixing tapable file

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class CA extends Component {
+  render() {
+    return <h1>Hola Mundo</h1>;
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: ["babel-polyfill", path.resolve(__dirname, "src/index.js")],
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["es2015", "react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                modules: true,
+                importLoaders: 1
+              }
+            },
+            "postcss-loader"
+          ]
+        })
+      },
+      {
+        test: /\.(jpg|png|gif|svg)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000000,
+            name: "img/BMW/[name].[ext]"
+          }
+        }
+      },
+      {
+        test: /\.(mp4)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            limit: 1000000,
+            mimetype: "video/mp4",
+            name: "video/[name].[hash].[ext]"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("css/[name].css"),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve(__dirname, "src/html/index.html"),
+      title: "Jumbo page"
+    })
+  ]
+};
